c73213c8628b8bece0e0c318f60b2770531e8038
Gokul-nath-K <gokulnathk003@gmail.com>
2023-04-19 23:45:55 +0530

    Update router.js

--- url-shortner backend/src/Routers/router.js
+++ url-shortner backend/src/Routers/router.js
@@ -1,10 +1,11 @@
 import express from "express";
 
 import { getUrl, shortenUrl } from "../Controllers/controller.js";
+import { validateUrl } from "../Validators/urlValidator.js";
 
 const router = express.Router();
 
-router.post('/shorten', shortenUrl);
+router.post('/shorten',validateUrl, shortenUrl);
 router.get('/:urlCode', getUrl);
 
 export default router;
